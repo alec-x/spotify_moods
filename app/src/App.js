8d020c6dc6afea7ade54f1e699d1891e90c7f981
Jeff Y <jyeung.eng@gmail.com>
2022-06-10 14:50:11 -0700

    Add global reset css

--- app/src/App.js
+++ app/src/App.js
@@ -3,26 +3,55 @@ import { Routes, Route } from 'react-router-dom';
 import LandingScreen from './screens/Landing/LandingScreen';
 import MoodPlaylistScreen from './screens/Playlist/PlaylistScreen';
 import SearchScreen from './screens/Search/SearchScreen';
-import { createTheme, NextUIProvider } from '@nextui-org/react';
+import { globalCss, createTheme, NextUIProvider } from '@nextui-org/react';
 
 const darkTheme = createTheme({
   type: 'dark',
   theme: {
     color: {
-      background: '#121217'
-    }
-  }
+      background: '#121217',
+    },
+  },
+});
+
+// Applied Global CSS style reset from 
+// https://www.joshwcomeau.com/css/custom-css-reset/
+const globalCssReset = globalCss({
+  '*': { boxSizing: 'border-box', margin: 0 },
+  '*::before': { boxSizing: 'border-box' },
+  '*::after': { boxSizing: 'border-box' },
+  html: { height: '100%' },
+  body: { height: '100%', lineHeight: 1.5 },
+  img: {display: 'block', maxWidth: '100%'},
+  picture: {display: 'block', maxWidth: '100%'},
+  video: {display: 'block', maxWidth: '100%'},
+  canvas: {display: 'block', maxWidth: '100%'},
+  svg: {display: 'block', maxWidth: '100%'},
+  input: {font: 'inherit'},
+  button: {font: 'inherit'},
+  textarea: {font: 'inherit'},
+  select: {font: 'inherit'},
+  p: { overflowWrap: 'break-word'},
+  h1: { overflowWrap: 'break-word'},
+  h2: { overflowWrap: 'break-word'},
+  h3: { overflowWrap: 'break-word'},
+  h4: { overflowWrap: 'break-word'},
+  h5: { overflowWrap: 'break-word'},
+  h6: { overflowWrap: 'break-word'},
+  '#root': {isolation: 'isolate'},
+  '#__next': {isolation: 'isolate'}
 });
 
 function App() {
+  globalCssReset();
   return (
-      <NextUIProvider theme={darkTheme}>
-        <Routes>
-          <Route path="/" element={<LandingScreen />} />
-          <Route path="/search" element={<SearchScreen />} />
-          <Route path="/playlist" element={<MoodPlaylistScreen />} />
-        </Routes>
-      </NextUIProvider>
+    <NextUIProvider theme={darkTheme}>
+      <Routes>
+        <Route path="/" element={<LandingScreen />} />
+        <Route path="/search" element={<SearchScreen />} />
+        <Route path="/playlist" element={<MoodPlaylistScreen />} />
+      </Routes>
+    </NextUIProvider>
   );
 }
 
