7ae3948a6cf497334462b25e43df5b6f07940eaa
stregaa <markus_chung@hotmail.com>
2022-06-21 19:25:42 -0700

    updated landing screen and search screen

--- app/src/screens/Search/SearchScreen.js
+++ app/src/screens/Search/SearchScreen.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Container } from '@nextui-org/react';
+import { Container, Button, Input, Row, Loading } from '@nextui-org/react';
 
 const SearchScreen = () => {
   return (
     <Container md>
-      <h1>SearchScreen</h1>
       <Link to="/playlist">Playlist</Link>
+      <Row>
+        <Input css={{ top:50 }} width="1000px" clearable bordered labelPlaceholder="Search for your song.."></Input>
+        <Button css={{ top:50, left: 10 }} color="success">Search</Button>
+      </Row>
+      <Container md>
+        <h3>Loading placeholder..</h3>
+        <Loading type="points" size="md" color="success"/>
+      </Container>
     </Container>
   );
 };
