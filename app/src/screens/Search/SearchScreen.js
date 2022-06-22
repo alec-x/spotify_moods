9f698481920db0942bfab059f006a2f3637ff403
stregaa <markus_chung@hotmail.com>
2022-06-21 19:36:14 -0700

    added loading dots

--- app/src/screens/Search/SearchScreen.js
+++ app/src/screens/Search/SearchScreen.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Container, Button, Input, Row, Loading } from '@nextui-org/react';
+import { Container, Button, Input, Row} from '@nextui-org/react';
+import LoadingDots from '../../common/LoadingDots';
 
 const SearchScreen = () => {
   return (
@@ -10,10 +11,7 @@ const SearchScreen = () => {
         <Input css={{ top:50 }} width="1000px" clearable bordered labelPlaceholder="Search for your song.."></Input>
         <Button css={{ top:50, left: 10 }} color="success">Search</Button>
       </Row>
-      <Container md>
-        <h3>Loading placeholder..</h3>
-        <Loading type="points" size="md" color="success"/>
-      </Container>
+      <LoadingDots />
     </Container>
   );
 };
