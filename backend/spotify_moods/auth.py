cf4b1b85a0483a10f5366175e9ba4d239443fb1c
alec xu <alec_xu@hotmail.com>
2022-11-25 00:08:32 -0800

    clean up auth functions

--- backend/spotify_moods/auth.py
+++ backend/spotify_moods/auth.py
@@ -1,51 +1,47 @@
 from urllib.parse import urlencode as urlencode
 import uuid
-
 import json
-
-from requests.api import request
 import requests
 import base64
 
-def _export_secrets(secrets_path: str) -> None:
-        with open(secrets_path, 'r') as f:
-            data = json.load(f)
+SECRET_FILE = "id.json"
+AUTH_URL = f"https://accounts.spotify.com/authorize"
+TOKEN_URL = f"https://accounts.spotify.com/api/token"
 
+def _export_secrets(secrets_path: str) -> None:
+        with open(secrets_path, "r") as f:
+            data = json.load(f)
         return data
 
-def get_sign_in_url(scope: str, user: str) -> str:
-    
-    # username = 'alecxu' # use this for user-state redis later
+def get_sign_in_url(scope: str) -> str:
     state = str(uuid.uuid4())
-
-    secrets = _export_secrets('id.json')
+    secrets = _export_secrets(SECRET_FILE)
     params = {    
-        'response_type': 'code',
-        'client_id': secrets["client_id"],
-        'scope': scope,
-        'redirect_uri': secrets["redirect_uri"],
-        'state': state,
-        'show_dialog' : True}
+        "response_type": "code",
+        "client_id": secrets["client_id"],
+        "scope": scope,
+        "redirect_uri": secrets["redirect_uri"],
+        "state": state,
+        "show_dialog" : True}
     params = urlencode(params)
-    return 'https://accounts.spotify.com/authorize?' + params, state
+    return AUTH_URL + "?" + params, state
 
 def post_request_auth_token(code) -> requests.post:
-    secrets = _export_secrets('id.json')
-    url = 'https://accounts.spotify.com/api/token'
+    secrets = _export_secrets(SECRET_FILE)
+    url = TOKEN_URL
     data = {    
-        'grant_type': 'authorization_code',
-        'code': code,
-        'redirect_uri': secrets["redirect_uri"]
+        "grant_type": "authorization_code",
+        "code": code,
+        "redirect_uri": secrets["redirect_uri"]
         }
     
-    client_id = secrets['client_id']
-    client_secret = secrets['client_secret']
-    client_creds = (client_id + ':' + client_secret).encode("ascii")
-    auth_str =  base64.b64encode(client_creds).decode('ascii')
+    client_creds = (secrets["client_id"] + ":" + secrets["client_secret"]).encode("ascii")
+    auth_str =  base64.b64encode(client_creds).decode("ascii")
     headers = {
-        'Authorization': 'Basic ' + auth_str,
-        'Content-Type': 'application/x-www-form-urlencoded'
+        "Authorization": "Basic " + auth_str,
+        "Content-Type": "application/x-www-form-urlencoded"
     }
+    
     post_request = requests.post(url, data=urlencode(data), headers=headers)
     if post_request.status_code == requests.codes.ok:
         return json.loads(post_request.text)
