1ef07771766798eae2f377986252e8323cc047de
alec xu <alec_xu@hotmail.com>
2022-11-28 14:57:10 -0800

    implemented get audio features

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI, HTTPException, Response
+from fastapi import FastAPI, HTTPException, Query, Response
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse, RedirectResponse
 
 import logging
 from logging.config import dictConfig
+
+from pydantic import BaseModel
 from log_conf import log_config
 
 import redis
@@ -12,7 +14,13 @@ import redis
 import spotify_moods.auth as smoods_auth
 import spotify_moods.api as smoods_api
 from spotify_moods.data import spotify_song, spotify_image
-from typing import List
+from typing import Dict, List, Union
+
+class Item(BaseModel):
+    name: str
+
+class StrList(BaseModel):
+    items: List[Item]
 
 dictConfig(log_config)
 app = FastAPI(debug=True)
@@ -79,6 +87,11 @@ def sign_in_callback(state: str = None, code: str = None, error: str = None) -> 
 def get_all_songs(state: str) -> List[spotify_song]:
     token = get_token(state, db)
     songs = smoods_api.get_all_songs(token)
-    logger.debug(len(songs))
     return songs
 
+@app.get("/api/features", response_model=List[Dict])
+def get_song_features(state: str, ids: Union[list[str], None] = Query(default=None)):
+    token = get_token(state, db)
+    print(token, ids)
+    features = smoods_api.get_audio_features(token, ids)
+    return features
--- backend/spotify_moods/api.py
+++ backend/spotify_moods/api.py
@@ -1,4 +1,5 @@
 
+from typing import List
 import warnings
 warnings.filterwarnings("ignore")
 from .data import spotify_image, spotify_song
@@ -58,64 +59,35 @@ def get_all_songs(token: str) -> list[dict]:
     
     return results
 
-    while offset < num_songs_total:
-        # compile list to give query
-        print(f"Processing batch: {offset}/{num_songs_total}", end='\r')
-
-        curr_size = min(batch_size, num_songs_total - offset)
-
-        uri_list = uris[offset:offset+curr_size]
+def get_audio_features(token: str, ids: list[str]) -> list[dict]:
+    QUERY_URL = "https://api.spotify.com/v1/audio-features"
+    BATCH = 100
+    headers = _form_headers(token)
+    num_ids = len(ids)
+    results = []
+    offset = 0
+    
+    while offset < num_ids:
+        curr_size = min(BATCH, num_ids - offset)
+        uri_list = ids[offset:offset+curr_size]
+        params = {"ids": ','.join(uri_list)}
         # conduct query
-        curr_batch = sp_client.audio_features(uri_list)
-
+        query = requests.get(QUERY_URL, params, headers=headers)
+        curr_features = json.loads(query.text)["audio_features"]
         # associate query result with existing results dictionary
-        keys_i_hate = ['id', 'type', 'uri', 'track_href', 'analysis_url', 'duration_ms', 'time_signature']
+        keys_i_hate = ["id", "type", "uri", "track_href", "analysis_url", "duration_ms", "time_signature"]
         for i in range(curr_size):
             for key in keys_i_hate:
-                del curr_batch[i][key]
-            results.append(curr_batch[i])  
+                del curr_features[i][key]
+            results.append(curr_features[i])  
 
-        # increment batch (can also be done outside of loop)
-        offset += batch_size
+        # increment BATCH (can also be done outside of loop)
+        offset += BATCH
     
     return results
 
-
-def get_all_songs(state) -> list[dict]:
-
-    results = []
-
-    offset = 0
-    num_songs_total = sp_client.current_user_saved_tracks(limit=1)['total']
-
-    # Information returned in batches in dictionary
-    while offset < num_songs_total:
-        print(f"Processing batch: {offset}/{num_songs_total}", end='\r')
-        curr_batch = sp_client.current_user_saved_tracks(limit=50, offset=offset)['items']
-        for track in curr_batch:
-            track = track['track']
-            curr_artists = [artist['name'] for artist in track['artists']]
-            curr_images = [spotify_image(img['height'], img['width'], img['url']) for img in track['album']['images']]
-
-            curr_song = spotify_song(track['id'], 
-                                     track['name'], 
-                                     curr_artists, 
-                                     track['album']['id'], 
-                                     track['href'], 
-                                     track['uri'],
-                                     curr_images)
-            results.append(curr_song)
-            
-        offset += 50
-    
-    print("\nFinished loading liked songs")
-    return results
-"""
-
-
-def create_playlist_for_user(sp_client: spotipy.Spotify, playlist_name: str, username: str, song_ids: list[str]) -> None:
+def create_playlist_for_user(token: str, playlist_name: str, username: str, song_ids: List[str]) -> None:
     playlist = sp_client.user_playlist_create(username, playlist_name, public=True, collaborative=False, description="testing create playlist function")
-    playlist_results = sp_client.user_playlist_add_tracks(username, playlist['id'], song_ids)
+    playlist_results = sp_client.user_playlist_add_tracks(username, playlist["id"], song_ids)
 
     return playlist_results
-"""
